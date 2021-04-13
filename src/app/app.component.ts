568c71b2b552581879f8fcf351fc695a05d9cb60
Nofar Edan <noedan@microsoft.com>
2021-04-13 21:14:21 +0300

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  value = 'World';
+export class AppComponent implements AfterViewInit{
+  title = 'ava-angular-app';
+
+  @ViewChild('rvxWidget', { static: false }) private RVXWidget;
+
+  public videoWidth = '';
+  public videoHeight = '';
+
+  public apiBase = '';
+
+  public widgetConfig  = {
+    token: '',
+    accountId: 'fe1bfcde-2787-4621-b20c-a70fc4b53464',
+    longRegionCode: 'mint1',
+    videoName: 'rodrigb-camera006'
+  };
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.RVXWidget.nativeElement.addEventListener('TOKEN_EXPIRED', () => {
+      console.log('token expired');
+    });
+    this.RVXWidget.nativeElement.configure(this.widgetConfig);
+  }
+
+  public setAPIBase() {
+    const rvx  = this.RVXWidget.nativeElement;
+    rvx.apiBase = this.apiBase;
+  }
+
+  public render() {
+    this.RVXWidget.nativeElement.configure(this.widgetConfig);
+    this.RVXWidget.nativeElement.render();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
