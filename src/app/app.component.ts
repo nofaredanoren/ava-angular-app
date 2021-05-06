bc6795d35c035f3dd4791a840632bbf9f957dcd3
Nofar Edan <noedan@microsoft.com>
2021-05-06 14:53:24 +0300

    update widget version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,17 @@ export class AppComponent implements AfterViewInit{
 
   @ViewChild('avaPlayer', { static: false }) private avaPlayer;
 
-  public videoWidth = '';
+  public videoWidth = '700px';
   public videoHeight = '';
 
   public apiBase = '';
   public accessToken = '';
 
   public widgetConfig  = {
-    token: '',
-    accountId: 'fe1bfcde-2787-4621-b20c-a70fc4b53464',
+    token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjFzemVmSnlHSWlTSGNWcWQ4Q041WVNGaGptOCIsIng1dCI6IjFzemVmSnlHSWlTSGNWcWQ4Q041WVNGaGptOCIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2MTk2MzgyNzIsImV4cCI6MTY1MTE3NDI2OSwiaWF0IjoxNjE5NjM4MjcyLCJpc3MiOiJTYW1wbGVUb2tlblByb3ZpZGVyIiwiYXVkIjoidGVzdCJ9.gznXMKIqxoxH_I9Nf-JWwiwJ926joeJfysbVcUvEveJWDhSNCuods0ELUvjReQgKr150Pe1dTZYXYU6NKDM6A1UK3KWQW1vf7iUTmf8FfKgMyUvQq_BzlCfV79WRy4XK2vY49wIIw3XdxiXbJhoMiHaezt678-Fn7mzoG6aR1d_P8rIqGDinAY1-9ryvB4Aoq8mC7MP9ZHnDZ4Q_8dLEYmnuOK4R21kOHFkLa6D8wxAdrKPrRt7CtktlKiE-8WVhJKjIFW92PHIaYEx4I14L5D0AiCLfH4DFEsVVlST7vOLuNxcgRx_t0aw10EGmntHqW-UHOlCa8xuB4t25c_imvg',
+    accountId: 'b43e0a4333ff49599b5f6c213b8626f2',
     longRegionCode: 'mint1',
-    videoName: 'rodrigb-camera006'
+    videoName: 'live-yolov3-tracker'
   };
 
   constructor() {
