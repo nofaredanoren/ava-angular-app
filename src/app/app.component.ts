bf9bab7fc086924d678ec77604e6700dd2a29ad4
Nofar Edan <noedan@microsoft.com>
2021-06-29 19:06:04 +0300

    Update options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,92 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements AfterViewInit{
-  title = 'ava-angular-app';
+export class AppComponent implements AfterViewInit {
+  title = "ava-angular-app";
 
-  @ViewChild('avaPlayer', { static: false }) private avaPlayer;
-  @ViewChild('avaPlayerDrawer', { static: false }) private avaPlayerDrawer;
-  @ViewChild('zoneDrawer', { static: false }) private zoneDrawer;
+  @ViewChild("avaPlayer", { static: false }) private avaPlayer;
+  @ViewChild("avaPlayerDrawer", { static: false }) private avaPlayerDrawer;
+  @ViewChild("zoneDrawer", { static: false }) private zoneDrawer;
 
-  public videoWidth = '700px';
-  public videoHeight = '';
+  public videoWidth = "700px";
+  public videoHeight = "";
 
-  public apiBase = '';
-  public accessToken = '';
+  public zoneDrawWidth = "700px";
 
-  public widgetConfig  = {
-    token: '',
-    clientApiEndpointUrl: '',
-    videoName: ''
+  public apiBase = "";
+  public accessToken = "";
+
+  public theme = 'dark';
+
+  public selectAllControllers = true;
+
+  public allowedControllers = {
+    rewind : true,
+    play_pause : true,
+    fast_forward : true,
+    live : true,
+    mute : true,
+    volume : true,
+    meta_data_layer : true,
+    overflow_menu : true,
+    fullscreen : true,
+    next_day : true,
+    previous_day: true,
+    hours_label : true,
+    next_segment : true,
+    prev_segment : true,
+    date_picker : true,
+    camera_name : true,
+    timestamp : true,
+    timeline_zoom : true
+  }
+
+  public widgetConfig = {
+    token: "",
+    clientApiEndpointUrl: "",
+    videoName: "",
+    locale: 'en',
+    playerControllers : undefined
   };
 
-  constructor() {
-  }
+  public widgetConfigZoneDrawerPlayer = {
+    token: "",
+    clientApiEndpointUrl: "",
+    videoName: "",
+    locale: 'en'
+  };
+
+  constructor() {}
 
   ngAfterViewInit(): void {
     // this.zoneDrawer.nativeElement.width = this.videoWidth;
-    this.avaPlayer.nativeElement.width=this.videoWidth;
-    this.avaPlayer.nativeElement.addEventListener('TOKEN_EXPIRED', () => {
-      console.log('token expired');
+    this.avaPlayer.nativeElement.width = this.videoWidth;
+    this.avaPlayer.nativeElement.addEventListener("TOKEN_EXPIRED", () => {
+      console.log("token expired");
     });
+
   }
 
   public setAPIBase() {
-    const rvx  = this.avaPlayer.nativeElement;
+    const rvx = this.avaPlayer.nativeElement;
     rvx.apiBase = this.apiBase;
+  }
+
+  public selectAllOfControllers() {
+    for (const iterator of Object.keys(this.allowedControllers)) {
+      this.allowedControllers[iterator] = this.selectAllControllers;
+    }
+  }
+
+  public controllerChange() {
+    this.selectAllControllers  = true;
+    for (const iterator of Object.keys(this.allowedControllers)) {
+      this.selectAllControllers = this.selectAllControllers && this.allowedControllers[iterator];
+    }
   }
 
   public setAccessToken() {
@@ -47,32 +96,49 @@ export class AppComponent implements AfterViewInit{
   }
 
   public load() {
+    // Configure allowed controllers
+    if (!this.selectAllControllers) {
+      this.widgetConfig.playerControllers = [];
+      for (const iterator of Object.keys(this.allowedControllers)) {
+        if (this.allowedControllers[iterator]) {
+          this.widgetConfig.playerControllers.push(iterator);
+        }
+      }
+    } else {
+      this.widgetConfig.playerControllers = undefined;
+    }
     this.avaPlayer.nativeElement.configure(this.widgetConfig);
     this.avaPlayer.nativeElement.load();
+  }
+
+  public loadZoneDrawer() {
+    // Configure zone draw
+    this.zoneDrawer.nativeElement.configure({
+      debug: true,
+      locale: this.widgetConfigZoneDrawerPlayer.locale
+    });
 
     this.avaPlayerDrawer.nativeElement.configure({
-      ...this.widgetConfig,
+      ...this.widgetConfigZoneDrawerPlayer,
       debug: true,
       playerControllers: [
         'rewind',
         'play_pause',
         'fast_forward',
+        'live',
         'mute',
         'volume',
-        'spacer',
         'next_day',
         'previous_day',
-        'hours_label'
-      ]
+        'hours_label',
+        'overflow_menu',
+        'date_picker',
+        'camera_name',
+        'timestamp',
+        'timeline_zoom'
+      ],
     });
     this.avaPlayerDrawer.nativeElement.load();
-
-    // Configure zone draw
-    this.zoneDrawer.nativeElement.configure({
-      debug: true
-    });
-
-
 
     this.zoneDrawer.nativeElement.load();
   }
